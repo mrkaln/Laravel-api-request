c4b9d6d134304809f7bcc1c07d60c88d14947628
Mark <mallenr@gmail.com>
2025-04-27 14:47:29 -0400

    Format code

--- laravel_api_request.php
+++ laravel_api_request.php
@@ -2,7 +2,6 @@
 
 $client = new \GuzzleHttp\Client();
 
-
 $response = $client->request('GET', 'https://imdb232.p.rapidapi.com/api/actors/get-bio?nm=nm0000573', [
 	'headers' => [
 		'x-rapidapi-host' => 'imdb232.p.rapidapi.com',
@@ -14,7 +13,7 @@ $data = json_decode($response->getBody(), true);
 $actorName = $data['data']['name']['nameText']['text'];
 $actorimage = $data['data']['name']['primaryImage']['url'];
 
-//output the image
+//output the image/
 echo "<img class='h-24 w-24' src='$actorimage' height='100px' width='100px'";
 
 ?>
